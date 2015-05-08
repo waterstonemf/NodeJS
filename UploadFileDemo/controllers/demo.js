0f5fdadb337230ea19a992f057ffde5ca5412302
waterstonemf <waterstonemf@outlook.com>
2015-05-08 18:00:44 +0800

    AddUploadFileDemo_BackboneDemo

---
+++ UploadFileDemo/controllers/demo.js
@@ -0,0 +1,41 @@
+exports.loadDemo = function(req,res){
+	res.render('demo',{});
+	//res.send({title:"none"});
+	
+}
+
+exports.showMore = function(req,res){
+	
+	var pageIndex = req.params.pageIndex;
+	console.log(pageIndex);
+	var content = "";
+	switch (parseInt(pageIndex)){
+	case 1:
+			content = "<div><img src='images/11.png' /></div><div><img src='images/12.png' /></div>";
+			console.log("I am in 1");
+			break;
+	case 2:
+			 content = "<div><img src='images/21.png' /></div><div><img src='images/22.png' /></div>";
+			break;
+	case 3:
+			 content = "<div><img src='images/31.png' /></div><div><img src='images/32.png' /></div>";
+			break;
+	default:
+		break;
+	}
+	
+	console.log(content);
+	res.send(content);
+	//res.send({title:"none"});
+	
+}
+
+exports.fileupload = function(req,res){
+
+	console.log(req.body);
+	console.log("below");
+	console.log(req.files)
+	res.send("end");
+	//res.send({title:"none"});
+	
+}
